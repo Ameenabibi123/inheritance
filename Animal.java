1f1872dacc29f1e777c43e7eeb0a4169ebc7f56d
Ameenabibi123 <ameenamahammadanavar8@gmail.com>
2025-04-15 10:08:00 +0530

    this is animal.java

---
+++ Animal.java
@@ -0,0 +1,14 @@
+//Superclass
+class Animal {
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+// Subclass inheriting from Animal
+class Dog extends Animal {
+    public void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
+
